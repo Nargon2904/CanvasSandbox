508ae51b1e6a42d1571b73a1a25f04620d6dd30e
Anthony <84743588+Nargon2904@users.noreply.github.com>
2024-04-06 15:52:51 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,90 @@
+const Canvas = document.getElementById('canvas');
+const ctx = Canvas.getContext('2d');
+
+Canvas.height = window.innerHeight;
+Canvas.width = window.innerWidth;
+
+//const imageData = ctx.createImageData(Canvas.width, Canvas.height);
+console.log(Canvas.height, Canvas.width);
+//ctx.putImageData(imageData, Canvas.width, Canvas.height);
+
+let imageData = 0;
+/*
+ctx.rect(10, 10, 100, 100);
+ctx.fill();
+
+let imageData = ctx.getImageData(60, 60, 200, 100);
+ctx.putImageData(imageData, 150, 10);
+*/
+
+
+
+let drawing = false;
+let posX = 0;
+let posY = 0;
+
+let arrX = [];
+let arrY = [];
+
+Canvas.addEventListener("mousedown", (e) => {
+    init(e);
+    drawing=true;
+})
+Canvas.addEventListener("mouseup", () => {
+    drawing=false; 
+    //imageData = ctx.getImageData(0, 0, Canvas.height, Canvas.width);
+    //ctx.putImageData(imageData, 150, 10);
+    //console.log(ctx.getImageData(0, 0, Canvas.height, Canvas.width));
+    console.log(arrX.min(), arrY.min(), arrX.max(), arrX.max());
+    arrX = [];
+    arrY = [];
+})
+Canvas.addEventListener("mousemove", (e) => {
+    draw(e);
+})
+
+function init(e) {
+    posX = e.offsetX;
+    posY = e.offsetY;
+}
+
+function draw(e) {
+    if (drawing) {
+        ctx.beginPath();
+        ctx.moveTo(posX, posY);
+        ctx.lineTo(e.offsetX, e.offsetY);
+        ctx.stroke();
+        ctx.closePath();
+        posX = e.offsetX;
+        posY = e.offsetY;
+
+        arrX.push(posX);
+        arrY.push(posY);
+        imageData = ctx.getImageData(0, 0, Canvas.height, Canvas.width);
+        ctx.putImageData(imageData, 1000, 10);
+    }
+}
+
+
+//кнопки
+
+const saveBtn = document.getElementById("save-btn");
+const clearBtn = document.getElementById("clear-btn");
+const blackBtn = document.getElementById('black-btn');
+const redBtn = document.getElementById('red-btn');
+const colorPicker = document.getElementById('color-picker-input');
+
+clearBtn.addEventListener('click', () => {
+    ctx.clearRect(0,0, Canvas.width, Canvas.height);
+})
+blackBtn.addEventListener('click', () => {
+    ctx.strokeStyle = '#000000';
+    colorPicker.value = '#000000';
+})
+redBtn.addEventListener('click', () => {
+    ctx.strokeStyle = '#FF0000';
+    colorPicker.value = '#FF0000';
+})
+colorPicker.addEventListener('change', (e) => {
+    ctx.strokeStyle = e.target.value;
+})
